76c79546f8c0958f772d54703842cc85129d68be
mingtaiha <ming.tai.ha@gmail.com>
2017-04-01 04:34:56 +0200

    adding exp setup

---
+++ exp/setup.sh
@@ -0,0 +1,5 @@
+source ~/ve/ve.exec_model_exp_devel/bin/activate
+export SAGA_PTY_SSH_TIMEOUT=60
+#export RADICAL_PILOT_VERBOSE=DEBUG
+export RADICAL_PILOT_DBURL=mongodb://mingtaiha:mingtaiha@ds053838.mlab.com:53838/hicomb
+export RADICAL_PILOT_PROFILE=TRUE
