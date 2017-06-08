e76fe93083a95605e76a96c7d340d51aa3a7a465
mingtaiha <ming.tai.ha@gmail.com>
2017-06-08 05:34:47 +0200

    compressing for bridges

---
+++ exp/model_confirm/raw_data/with_flops/1024_cus/bridges/aggr_data/aggr.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+python ../get_unit_tx.py rp.session.Ubuntu-1604-xenial-64-minimal.mingtha.017317.0019 > unit_tx_017317.0019
+python ../get_unit_tx.py rp.session.Ubuntu-1604-xenial-64-minimal.mingtha.017317.0020 > unit_tx_017317.0020
+python ../get_unit_tx.py rp.session.Ubuntu-1604-xenial-64-minimal.mingtha.017318.0000 > unit_tx_017318.0000
+python ../get_unit_tx.py rp.session.Ubuntu-1604-xenial-64-minimal.mingtha.017318.0001 > unit_tx_017318.0001
+python ../get_unit_tx.py rp.session.Ubuntu-1604-xenial-64-minimal.mingtha.017319.0000 > unit_tx_017319.0000
+python ../get_unit_tx.py rp.session.Ubuntu-1604-xenial-64-minimal.mingtha.017321.0000 > unit_tx_017321.0000
+python ../get_unit_tx.py rp.session.Ubuntu-1604-xenial-64-minimal.mingtha.017321.0001 > unit_tx_017321.0001
+python ../get_unit_tx.py rp.session.Ubuntu-1604-xenial-64-minimal.mingtha.017323.0000 > unit_tx_017323.0000
+python ../get_unit_tx.py rp.session.Ubuntu-1604-xenial-64-minimal.mingtha.017323.0007 > unit_tx_017323.0007
