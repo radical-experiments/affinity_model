d037d27f23c89b4c747cb6e92a35aac17d74f129
mingtaiha <ming.tai.ha@gmail.com>
2017-07-06 22:35:18 -0400

    preliminary analysis of amarel data looks p good

--- plots/kernel_timings/cleaned_data/amarel/kernel_stats_aggr.py
+++ plots/kernel_timings/cleaned_data/amarel/kernel_stats_aggr.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import csv
+import pprint
 
 import calc_basic_stats as cbs
 
@@ -12,18 +13,19 @@ print file_list
 
 out_filename = sys.argv[2]
 
-num_cycles = list()
-num_cycles_data = list()
-
 for filename in file_list:
     session_number = filename.split(".csv")[0].split('_')[-1]
+    output_data = list()
+    num_cycles = list()
+    num_cycles_data = list()
     with open(pathname+filename, 'r') as f:
         for row in f:
             row_data = row.split(',')
             num_cycles.append(int(row_data[0]))
             num_cycles_data.append(map(float, row_data[1:]))
 
-    output_data = [[out_filename, 'Average', 'Std', 'CI']]
+    title_data = [out_filename, 'Average', 'Std', 'CI']
+    output_data = list()
     for i in range(len(num_cycles)):
         mean = cbs.average(num_cycles_data[i])
         samp_stdev = cbs.samp_std_dev(num_cycles_data[i])
@@ -32,9 +34,12 @@ for filename in file_list:
         entry_data = [num_cycles[i], mean, samp_stdev, conf_intv]
         output_data.append(entry_data)
 
-    output_data = map(list, zip(*output_data))
+    output_data.sort(key=lambda x: x[0])
+    output_data.insert(0, title_data)
+    write_data = map(list, zip(*output_data))
+    pprint.pprint(write_data)
 
     with open(out_filename+"_"+session_number+".csv", 'w') as f:
         writer = csv.writer(f)
-        for row in output_data:
+        for row in write_data:
             writer.writerow(row)
