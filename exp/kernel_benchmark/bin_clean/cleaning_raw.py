82ff7589fc48a68a76d554b61506e3752ff13b6c
mingtaiha <ming.tai.ha@gmail.com>
2017-07-17 21:26:57 -0400

    automated aggregation of data, only analysis left

--- exp/kernel_benchmark/bin_clean/cleaning_raw.py
+++ exp/kernel_benchmark/bin_clean/cleaning_raw.py
@@ -2,6 +2,8 @@ import os
 import sys
 import csv
 
+from files_dir_mod import *
+
 
 perf_output_format = {
     "laptop" : [16, 3, 7, 10, 14],
@@ -12,7 +14,7 @@ perf_output_format = {
 def extract_from_perf(src_pathname, filename, dst_pathname, machine, kernel_type):
 
     data_array = list()
-    title = ['cpu_util', 'cycles', 'clkspeed', 'instr', 'instr_rate','time']
+    title = ['cpu_util', 'cycles_act', 'clkspeed', 'instr_act', 'instr_rate', 'time']
     data_array.append(title)
 
     with open(src_pathname+'/'+filename, 'r') as f:
@@ -85,44 +87,6 @@ def extract_from_perf(src_pathname, filename, dst_pathname, machine, kernel_type
             writer.writerow(row)
 
 
-
-dirnames = [
-            "adder/laptop/clkspeed_2-6",
-            "adder/laptop/clkspeed_3-5",
-            "adder/desktop/clkspeed_2-6",
-            "adder/desktop/clkspeed_3-5",
-            "matmult/laptop/clkspeed_2-6",
-            "matmult/laptop/clkspeed_3-5",
-            "matmult/desktop/clkspeed_2-6",
-            "matmult/desktop/clkspeed_3-5",
-            "adder/amarel/compute/exclusive",
-            "adder/amarel/compute/shared",
-            "adder/amarel/mem/exclusive",
-            "adder/amarel/mem/shared",
-            "matmult/amarel/compute/exclusive",
-            "matmult/amarel/compute/shared",
-            "matmult/amarel/mem/exclusive",
-            "matmult/amarel/mem/shared"
-            ]
-
-adder_filenames = [
-					"perfstat_1000000",
-					"perfstat_1000000000",
-					"perfstat_10000000000",
-					"perfstat_100000000000",
-					"perfstat_250000000000",
-					"perfstat_500000000000",
-					"perfstat_750000000000",
-					"perfstat_1000000000000"
-					]
-
-matmult_filenames = [
-						"perfstat_1000",
-						"perfstat_2500",
-						"perfstat_5000",
-						"perfstat_7500",
-						"perfstat_10000"
-					]
 
 def clean_data(src_path, dst_path):
 
---
+++ exp/kernel_benchmark/bin_clean/files_dir_mod.py
@@ -0,0 +1,56 @@
+dirnames = [
+            "adder/laptop/clkspeed_2-6",
+            "adder/laptop/clkspeed_3-5",
+            "adder/desktop/clkspeed_2-6",
+            "adder/desktop/clkspeed_3-5",
+            "matmult/laptop/clkspeed_2-6",
+            "matmult/laptop/clkspeed_3-5",
+            "matmult/desktop/clkspeed_2-6",
+            "matmult/desktop/clkspeed_3-5",
+            "adder/amarel/compute/exclusive",
+            "adder/amarel/compute/shared",
+            "adder/amarel/mem/exclusive",
+            "adder/amarel/mem/shared",
+            "matmult/amarel/compute/exclusive",
+            "matmult/amarel/compute/shared",
+            "matmult/amarel/mem/exclusive",
+            "matmult/amarel/mem/shared"
+            ]
+
+adder_filenames = [
+					"perfstat_1000000",
+					"perfstat_1000000000",
+					"perfstat_10000000000",
+					"perfstat_100000000000",
+					"perfstat_250000000000",
+					"perfstat_500000000000",
+					"perfstat_750000000000",
+					"perfstat_1000000000000"
+					]
+
+matmult_filenames = [
+						"perfstat_1000",
+						"perfstat_2500",
+						"perfstat_5000",
+						"perfstat_7500",
+						"perfstat_10000"
+					]
+
+cleaned_adder_filenames = [
+					"timings_1000000",
+					"timings_1000000000",
+					"timings_10000000000",
+					"timings_100000000000",
+					"timings_250000000000",
+					"timings_500000000000",
+					"timings_750000000000",
+					"timings_1000000000000"
+					]
+
+cleaned_matmult_filenames = [
+						"timings_1000",
+						"timings_2500",
+						"timings_5000",
+						"timings_7500",
+						"timings_10000"
+					]
