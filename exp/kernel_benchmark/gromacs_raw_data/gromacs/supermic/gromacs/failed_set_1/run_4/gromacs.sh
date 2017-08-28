abea8682b540bcfcb0ac6c553d3b145fdf665ad4
mingtaiha <ming.tai.ha@gmail.com>
2017-08-27 21:17:43 -0400

    collecting supermic data

---
+++ exp/kernel_benchmark/gromacs_raw_data/gromacs/supermic/gromacs/failed_set_1/run_4/gromacs.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+time=`date +%s`
+pwd=$1
+
+for nsteps in 1000 5000 10000 25000 50000 75000 100000 250000 500000 750000 1000000;
+do
+	for iter in {1..15};
+	do
+		gmx grompp -f $pwd/grompp_${nsteps}.mdp -c $pwd/input.gro -p $pwd/topol.top -o $pwd/out.tpr 2>> $pwd/grompp_out_$nsteps
+		perf stat gmx mdrun -ntomp 1 -s $pwd/out.tpr -c $pwd/out.gro 2>> $pwd/perf_stat_$nsteps
+	done
+done
