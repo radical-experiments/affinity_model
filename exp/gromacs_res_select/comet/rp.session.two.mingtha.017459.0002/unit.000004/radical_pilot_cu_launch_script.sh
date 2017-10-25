68bfb39d2c3f0e6735f38e0e405f48c9fce31c90
mingtaiha <ming.tai.ha@gmail.com>
2017-10-25 02:18:51 +0200

    adding gromacs timings for units for multi-unit on same node run

---
+++ exp/gromacs_res_select/comet/rp.session.two.mingtha.017459.0002/unit.000004/radical_pilot_cu_launch_script.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+
+# Environment variables
+export RP_SESSION_ID="rp.session.two.mingtha.017459.0002"
+export RP_PILOT_ID="pilot.0000"
+export RP_AGENT_ID="agent_0"
+export RP_SPAWNER_ID="agent.executing.0.child"
+export RP_UNIT_ID="unit.000004"
+export RP_GTOD="/home/mingtha/radical.pilot.sandbox/rp.session.two.mingtha.017459.0002/pilot.0000/gtod"
+export RP_PROF="/home/mingtha/radical.pilot.sandbox/rp.session.two.mingtha.017459.0002/pilot.0000/unit.000004/PROF"
+export RP_TMP="/scratch/$USER/$SLURM_JOBID"
+
+
+touch $RP_PROF
+echo "`$RP_GTOD`,unit_script,unit.000004,AGENT_EXECUTING,start_script," >> $RP_PROF
+
+# Change to unit sandbox
+cd /home/mingtha/radical.pilot.sandbox/rp.session.two.mingtha.017459.0002/pilot.0000/unit.000004
+echo "`$RP_GTOD`,unit_script,unit.000004,AGENT_EXECUTING,after_cd," >> $RP_PROF
+
+# Pre-exec commands
+echo "`$RP_GTOD`,unit_script,unit.000004,AGENT_EXECUTING,pre_start," >> $RP_PROF
+module purge ||  (echo "pre_exec failed"; false) || exit
+module load gromacs/2016.3 ||  (echo "pre_exec failed"; false) || exit
+echo "`$RP_GTOD`,unit_script,unit.000004,AGENT_EXECUTING,pre_stop," >> $RP_PROF
+
+# The command to run
+/home/mingtha/bin/gromacs_pinned.sh
+RETVAL=$?
+echo "`$RP_GTOD`,unit_script,unit.000004,AGENT_EXECUTING,after_exec," >> $RP_PROF
+
+# Exit the script with the return code from the command
+exit $RETVAL
